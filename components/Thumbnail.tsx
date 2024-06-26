0f18a086fadeb34bcf066610122ffa0c7e76e232
Zulaikha <zhanif02@gmail.com>
2024-06-26 19:28:55 +0100

    creating thumbnail component

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,14 +1,22 @@
-import { Movie } from "@/typing"
+import { Movie } from "@/typing";
+import Image from "next/image";
 
 interface Props {
-    movie: Movie
+  movie: Movie;
 }
-function Thumbnail({movie}:Props) {
+function Thumbnail({ movie }: Props) {
   return (
     <div>
-      Thumbnail
+      <Image
+        src={`https://image.tmdb.org/t/p/w500${
+          movie.backdrop_path || movie.poster_path
+        }`}
+        className="rounded-sm object-cover md:rounded"
+        layout="fill"
+        alt=""
+      />
     </div>
-  )
+  );
 }
 
-export default Thumbnail
+export default Thumbnail;
