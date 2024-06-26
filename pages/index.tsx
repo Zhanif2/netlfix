0ae9ba37953a0059a5b7e2578e1fdc8c870ab84e
Zulaikha <zhanif02@gmail.com>
2024-06-26 13:09:36 +0100

    adding icons on header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
-export default function Home() {
+import Header from "@/components/Header";
+import { NextPage } from "next";
+import Head from "next/head";
+
+const Home:NextPage = () =>{
   return (
-    <>
-      <h1 className="text-2xl text-green-500">test text</h1>
-    </>
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title> Home - Netflix</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header/>
+    </div>
   )
 }
+
+
+export default Home;
