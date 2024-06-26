b1d8a67078040f651fb4006e05523bd8ed9776cf
Zulaikha <zhanif02@gmail.com>
2024-06-26 18:01:31 +0100

    button styling complete

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,6 +3,7 @@ import { Movie } from "@/typing";
 import { useEffect, useState } from "react";
 import { baseUrl } from "@/constants/movie";
 import { FaPlay } from "react-icons/fa";
+import { InformationCircleIcon } from "@heroicons/react/solid";
 
 interface Props {
   netflixOriginals: Movie[];
@@ -37,9 +38,9 @@ function Banner({ netflixOriginals }: Props) {
         </p>
 
 
-        <div>
-          <button className="bannerButton"><FaPlay className="h-4 w-4 text-black md:h-7 md:w-7"/>Play</button>
-          <button className="bannerButton">More Info</button>
+        <div className="flex space-x-3">
+          <button className="bannerButton bg-white text-black"><FaPlay className="h-4 w-4 text-black md:h-7 md:w-7"/>Play</button>
+          <button className="bannerButton bg-[gray]/70">More Info <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8"/></button>
         </div>
     </div>
   );
