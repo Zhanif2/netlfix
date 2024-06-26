3289fb10243fe94803171e86dbae5c99f55857bb
Zulaikha <zhanif02@gmail.com>
2024-06-26 15:24:49 +0100

    title and description added for header banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,31 +1,40 @@
-import Image from 'next/image'
-import { Movie } from "@/typing"
-import { useEffect, useState } from "react"
-import { baseUrl } from '@/constants/movie'
+import Image from "next/image";
+import { Movie } from "@/typing";
+import { useEffect, useState } from "react";
+import { baseUrl } from "@/constants/movie";
 
 interface Props {
-  netflixOriginals : Movie[]
+  netflixOriginals: Movie[];
 }
-function Banner({netflixOriginals}: Props) {
-  const [movie, setMovie] = useState<Movie | null>(null)
+function Banner({ netflixOriginals }: Props) {
+  const [movie, setMovie] = useState<Movie | null>(null);
 
   useEffect(() => {
-    setMovie(netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)])
-  },[netflixOriginals])
+    setMovie(
+      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+    );
+  }, [netflixOriginals]);
 
-console.log(movie)
+  console.log(movie);
   return (
     <div>
-      <div className=' absolute top-0 left-0 z-10 h-[95vh] w-screen'>
+      <div className=" absolute top-0 left-0 -z-10 h-[95vh] w-screen">
         <Image
-        src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
-        layout='fill'
-        objectFit='cover'
-        alt=''
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          layout="fill"
+          objectFit="cover"
+          alt=""
         />
       </div>
+
+      <h1 className="text-2xl lg:text-7xl md:text-4xl font-bold">
+        {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+      <p className="max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
+        {movie?.overview}
+        </p>
     </div>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
