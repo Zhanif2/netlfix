82066b80b8cc65f80e129f06b25ddd8e23e68975
Zulaikha <zhanif02@gmail.com>
2024-06-26 13:31:11 +0100

    backgrond of landing page adjusted

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,18 @@ import Head from "next/head";
 
 const Home:NextPage = () =>{
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 
+    to-[#010511] lg:h-[140vh]">
       <Head>
         <title> Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header/>
+      <main>
+        <section>
+
+        </section>
+      </main>
     </div>
   )
 }
