fbb8952c9e642ad35eae092d0b4b121b9424f96d
Zulaikha <zhanif02@gmail.com>
2024-06-29 16:42:45 +0100

    login page img added

---
+++ pages/login.tsx
@@ -0,0 +1,24 @@
+import Head from "next/head"
+import Image from "next/image"
+
+function login() {
+  return (
+    <div className="relative flex h-screen w-screen flex-col bg-black md:items-center
+    md: justify-center md:bg-transparent">
+      <Head>
+        <title> Netflix</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Image
+        src="https://rb.gy/p2hphi"
+        alt=""
+        layout="fill"
+        className="-z-10 !hidden opacity-60 sm:!inline"
+        objectFit="cover"
+      />
+
+    </div>
+  )
+}
+
+export default login
