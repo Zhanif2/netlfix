7a8255c3771340f79180a78658c76725e35c7c7e
Zulaikha <zhanif02@gmail.com>
2024-06-29 16:53:46 +0100

    input styling done

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,10 +1,12 @@
-import Head from "next/head"
-import Image from "next/image"
+import Head from "next/head";
+import Image from "next/image";
 
 function login() {
   return (
-    <div className="relative flex h-screen w-screen flex-col bg-black md:items-center
-    md: justify-center md:bg-transparent">
+    <div
+      className="relative flex h-screen w-screen flex-col bg-black md:items-center
+    md: justify-center md:bg-transparent"
+    >
       <Head>
         <title> Netflix</title>
         <link rel="icon" href="/favicon.ico" />
@@ -17,8 +19,28 @@ function login() {
         objectFit="cover"
       />
 
+      <img
+        src="https://rb.gy/ulxxee"
+        className="absolute left-4 top-4 cursor-pointer object-contain md:left-10 md:top-6"
+        width={150}
+        height={150}
+      />
+
+
+      <form className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 
+      md:max-w-md md:px-14">
+        <h1>Sign In</h1>
+        <div className="space-y-4">
+            <label>
+            <input type="email" placeholder="Email" className="input"/>
+            </label>
+            <label>
+            <input type="password" placeholder="Password" className="input"/>
+            </label>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
-export default login
+export default login;
